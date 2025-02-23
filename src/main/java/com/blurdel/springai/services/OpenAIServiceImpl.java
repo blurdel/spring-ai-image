1f60f717c037990d24721d0b187215530e7a349f
blurdel <5648737+blurdel@users.noreply.github.com>
2025-02-23 10:59:12 -0700

    Implement the image service

--- src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
+++ src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
@@ -2,17 +2,36 @@ package com.blurdel.springai.services;
 
 import com.blurdel.springai.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 
 @RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
+    private final ImageModel imageModel;
+
     @Override
     public byte[] getImage(Question question) {
 
-        return null;
+        var options = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1024)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+//                .withQuality("hd") // default standard
+//                .withStyle("natural") // default vivid
+                .build();
+
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+
+        var imageResponse = imageModel.call(imagePrompt);
+
+        return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
     }
 
 }

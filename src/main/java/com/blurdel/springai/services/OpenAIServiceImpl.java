9498c8cc77687b94818fc8693cb67d3ad1c4e4fd
blurdel <5648737+blurdel@users.noreply.github.com>
2025-02-23 12:23:02 -0700

    Implemented the service for vision description

--- src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
+++ src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
@@ -2,13 +2,22 @@ package com.blurdel.springai.services;
 
 import com.blurdel.springai.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.model.Media;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -16,6 +25,23 @@ import java.util.Base64;
 public class OpenAIServiceImpl implements OpenAIService {
 
     private final ImageModel imageModel;
+
+    private final ChatModel chatModel;
+
+    @Override
+    public String getDescription(MultipartFile file) {
+
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .model(OpenAiApi.ChatModel.GPT_4_O.getValue())
+                .build();
+
+        var userMessage = new UserMessage("Explain what do you see in this picture?",
+                List.of(new Media(MimeTypeUtils.IMAGE_JPEG, file.getResource())));
+
+        ChatResponse response = chatModel.call(new Prompt(List.of(userMessage), options));
+
+        return response.getResult().getOutput().toString();
+    }
 
     @Override
     public byte[] getImage(Question question) {
@@ -33,11 +59,6 @@ public class OpenAIServiceImpl implements OpenAIService {
         var imageResponse = imageModel.call(imagePrompt);
 
         return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
-    }
-
-    @Override
-    public String getDescription(MultipartFile file) {
-        return "";
     }
 
 }
